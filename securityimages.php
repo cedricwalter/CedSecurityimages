18c1209801195e1138d61db87496408c38d2f49f
cedric walter <cedric.walter@gmail.com>
2012-05-02 10:27:28 +0200

    BUG:  if enabled for Contacts, it prevents a backend administrator from saving contact details in the backend. It fails with a captcha error. I assume this is because it is enforcing a captch entry, yet the backend cannot facilitate the display of the captcha.  Please advise on how this can be resolved as currently I haveto disabled secure images for contacts to allow admin to save the updates.

--- securityimages.php
+++ securityimages.php
@@ -23,66 +23,74 @@ jimport('joomla.error.log');
 class plgSystemSecurityImages extends JPlugin
 {
 
-   var $logger = null;
-   var $debug = false;
-    
-   static $activeForms = array('com_users.registration',
-				  'com_users.login',
-				  'com_users.reset_request',
-				  'com_users.remind',
-				  'com_users.profile',
-                  'com_contact.contact');
+    var $logger = null;
+    var $debug = false;
+
+    static $activeForms = array('com_users.registration',
+        'com_users.login',
+        'com_users.reset_request',
+        'com_users.remind',
+        'com_users.profile',
+        'com_contact.contact');
 
 
-	function __construct(&$subject, $config)
-	{
-		parent::__construct($subject, $config);
-        $this->debug= $this->get('debug','0');
-		$this->logger = JLog::getInstance('plg_system_securityimages.php', array('format' => "{DATE}\t{TIME}\t{COMMENT}"));
-	}
-
-    function debug($message) {
-        if ($this->debug) {
-			$this->logger->addEntry(array('comment' => "Plugin plg_system_securityimages is active"));
-		}
+    function __construct(&$subject, $config)
+    {
+        parent::__construct($subject, $config);
+        $this->debug = $this->get('debug', '0');
+        $this->logger = JLog::getInstance('plg_system_securityimages.php', array('format' => "{DATE}\t{TIME}\t{COMMENT}"));
     }
-	
-/**
-	 * 
-	 * Enter description here ...
-	 * @param unknown_type $form
-	 * @param unknown_type $data
-	 */
-	function onContentPrepareForm($form, $data){
-		$this->debug("Form name <".$form->getName().">") ;
-        
-        if (!($form instanceof JForm)) {
-			$this->_subject->setError('JERROR_NOT_A_FORM');
-            $this->debug("Form name <".$form->getName()."> not a JForm yet in Joomla! core") ;
-            return false;
-		}
-		$this->loadLanguage();
-		JForm::addFieldPath(dirname(__FILE__).'/fields');
-		JForm::addFormPath (dirname(__FILE__).'/forms');
 
-		if ($this->params->get('override', '1') == false &&
-            in_array($form->getName(), plgSystemSecurityImages::$activeForms) == false){
-			$this->debug("Form name <".$form->getName()."> Not in the list of active forms");
+    function debug($message)
+    {
+        if ($this->debug) {
+            $this->logger->addEntry(array('comment' => "Plugin plg_system_securityimages is active"));
+        }
+    }
+
+    /**
+     *
+     * Enter description here ...
+     * @param unknown_type $form
+     * @param unknown_type $data
+     */
+    function onContentPrepareForm($form, $data)
+    {
+        $app = JFactory::getApplication();
+        if ($app->getName() != 'site') {
             return true;
-		}
+        }
 
-		if ( $this->get('override') ||
+        $this->debug("Form name <" . $form->getName() . ">");
+
+        if (!($form instanceof JForm)) {
+            $this->_subject->setError('JERROR_NOT_A_FORM');
+            $this->debug("Form name <" . $form->getName() . "> not a JForm yet in Joomla! core");
+            return false;
+        }
+        $this->loadLanguage();
+        JForm::addFieldPath(dirname(__FILE__) . '/fields');
+        JForm::addFormPath(dirname(__FILE__) . '/forms');
+
+        if ($this->params->get('override', '1') == false &&
+            in_array($form->getName(), plgSystemSecurityImages::$activeForms) == false
+        ) {
+            $this->debug("Form name <" . $form->getName() . "> Not in the list of active forms");
+            return true;
+        }
+
+        if ($this->get('override') ||
             ($form->getName() == "com_users.registration") && $this->params->get('register') ||
-		    ($form->getName() == 'com_users.login') && $this->params->get('login', '1') ||
-		    ($form->getName() == 'com_users.reset_request') && $this->params->get('reset', '1') ||
-			($form->getName() == 'com_users.remind') && $this->params->get('remind', '1') ||
-			($form->getName() == 'com_users.profile') && $this->params->get('contact', '1') ||
+            ($form->getName() == 'com_users.login') && $this->params->get('login', '1') ||
+            ($form->getName() == 'com_users.reset_request') && $this->params->get('reset', '1') ||
+            ($form->getName() == 'com_users.remind') && $this->params->get('remind', '1') ||
+            ($form->getName() == 'com_users.profile') && $this->params->get('contact', '1') ||
             ($form->getName() == 'com_contact.contact') && $this->params->get('contact', '1')
-		    ) {
-		    $this->debug("Add now securityimages to the Form name <".$form->getName().">");
-            $params 	= new JParameter(JPluginHelper::getPlugin('system', 'securityimages')->params);
-		    $form->loadFile($params->get('captchaType', 'nucaptcha'), false);
-		}
-		return true;
-	}
+        ) {
+            $this->debug("Add now securityimages to the Form name <" . $form->getName() . ">");
+            $params = new JParameter(JPluginHelper::getPlugin('system', 'securityimages')->params);
+            $form->loadFile($params->get('captchaType', 'nucaptcha'), false);
+        }
+        return true;
+    }
 }
